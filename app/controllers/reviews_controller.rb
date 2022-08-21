d6371c949eacc48fc24093d278fe912ae4425049
mariadianacalugaru <calugaru.1893272@studenti.uniroma1.it>
2022-08-21 22:28:54 +0200

    MVC review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+			redirect_to reviews_path, notice: "Review successfully added." 
+		else
+			render :new, status: :unprocessable_entity 
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:body)
+    end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root 'home#index'
   get 'home/index'
+
+  resources :reviews, only: [:index, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
