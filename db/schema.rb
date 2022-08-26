b860ef2ee867637aa2fe1ee80a5be374bbd119ce
Rosapia Laudati <rosilaudati@gmail.com>
2022-08-26 23:07:12 +0200

    aggiunto peso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_180335) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_094458) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_180335) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "bodyPart"
+    t.string "equipment"
+    t.string "gifUrl"
+    t.string "name"
+    t.string "target"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "requests", force: :cascade do |t|
