92a66c14631041c4deafac1a0aee4a820059e6ec
Rosapia Laudati <rosilaudati@gmail.com>
2022-08-27 00:40:29 +0200

    visualizzazione e creazione richieste

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'omniauth-google-oauth2'
 gem "omniauth-rails_csrf_protection"
 
 gem 'cancancan'
-
+gem 'net-http'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
